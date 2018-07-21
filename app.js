38beaad7f5978f8a5accb2e6f725ee5a76eb43c0
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-07-21 02:44:13 +0200

    Add cat names to cards

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-let catConts = document.getElementsByClassName('cat-cont');
+let catConts = document.getElementsByClassName('cat-cont'),
+    catNames = ['Zbeda', 'Bosy'],
+    namesHeaders = document.getElementsByClassName('name');
+
+for(let i=0; i<namesHeaders.length; i++) {
+    namesHeaders[i].innerText = catNames[i];
+}
 
 function incrementCounter() {
     let counter = this.parentElement.querySelector('.counter');
